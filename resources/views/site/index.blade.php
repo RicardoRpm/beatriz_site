0617d5d3ac0e48caf218f0821f0529c5b93e11c0
RicardoRpm <Ricardomiguel190@gmail.com>
2024-06-25 07:48:09 +0100

    Update load of product image

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -510,7 +510,7 @@
                   </div>
 
                   <div class="col-xl-5">
-                    <div class="card-bg" style="background-image: url('{{ asset("assets/img/granito.jpg") }}') "></div>
+                    <div class="card-bg" style="background-image: url('/assets/img/granito.jpg') "></div>
                   </div>
 
                 </div>
