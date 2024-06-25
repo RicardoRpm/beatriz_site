84727e966b152bfff290748c18ba8b1576bc3e07
RicardoRpm <Ricardomiguel190@gmail.com>
2024-06-25 06:32:26 +0100

    Enable preload

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -223,10 +223,8 @@
     </footer>
     <!-- End Footer -->
 
-    <!-- 
     <div id="preloader"></div>
-    -->
-
+    
     <a href="#" class="scroll-top d-flex align-items-center justify-content-center">
         <i class="bi bi-arrow-up-short"></i>
     </a>
