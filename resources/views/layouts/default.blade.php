751b42064ae0f00b8ffd88196be1a015c5a80afa
RicardoRpm <Ricardomiguel190@gmail.com>
2024-08-08 05:35:59 +0100

    svg

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -109,8 +109,7 @@
 
                     <div class="col-md-4">
                         <div class="footer-info">
-                            <image src="assets/img/logo.svg" style="width: 12rem;" /> 
-                            {{-- <image src="{{ URL::asset('assets/img/logo.svg') }}" style="width: 12rem;" />  --}}
+                            <image src="{{ URL::asset('assets/img/logo.svg') }}" style="width: 12rem;" /> 
                             <br>
                             <br>
                             @switch(session('locale'))
